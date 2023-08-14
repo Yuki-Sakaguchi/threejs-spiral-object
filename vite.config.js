4432bc4139f54d0a698b3906b53a5219d5b49d44
Yuki Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2023-08-14 22:02:23 +0900

    Add distにデプロイ

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: { port: 3000, },
+  server: { port: 3000 },
   root: "src",
+  build: {
+    outDir: "../dist",
+  },
 });
